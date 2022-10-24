b643cdb04dc81f8cb0082d718a927cafcd564601
Jonas Claes <jonas@jonasclaes.be>
2022-10-24 12:17:43 +0200

    Driver for shooter car

---
+++ code/3DI/shooterdriver/shooterdriver.c
@@ -0,0 +1,7 @@
+#include <linux/module.h>
+#include <linux/pwm.h>
+
+MODULE_AUTHOR("Jonas Claes <jonas@jonasclaes.be>");
+MODULE_DESCRIPTION("Shooter Driver");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:shooter");
