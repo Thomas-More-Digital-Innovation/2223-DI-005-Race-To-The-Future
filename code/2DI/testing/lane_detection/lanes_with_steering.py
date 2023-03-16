5a950b8b4b4f26cb58fc71ec63e92d153392f299
rttf-2 <rttf-2@rttf-2.com>
2023-03-16 15:33:11 +0100

    small fix

--- code/2DI/testing/lane_detection/lanes_with_steering.py
+++ code/2DI/testing/lane_detection/lanes_with_steering.py
@@ -23,7 +23,7 @@ def average_slope_intercept(image, lines):
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
-            parameters = np.polyfit((x1, ((x1+x2)/2), x2), (y1,((y1+y2)/2),  y2), 1)
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
             slope = parameters[0]
             intercept = parameters[1]
             if slope < 0:
