3bf4ae97ffb5d982de46de7cc1ae0195f38563a4
rttf-2 <rttf-2@rttf-2.com>
2023-03-30 16:25:49 +0200

    uturn was a horrible mistakeµ

--- code/2DI/run.py
+++ code/2DI/run.py
@@ -4,8 +4,19 @@ from shooterdriver import ShooterDriver
 
 driver = ShooterDriver()
 
-driver.set_steering_angle(1.0)
-sleep(1)
-driver.set_steering_angle(-1.0)
-sleep(1)
-driver.set_steering_angle(0.0)
+# driver.set_steering_angle(1.0)
+# sleep(1)
+# driver.set_steering_angle(-1.0)
+# sleep(1)
+# driver.set_steering_angle(0.0)
+
+try:
+    driver.set_wheels_speed(-1.0)
+    sleep(1)
+    driver.set_wheels_speed(0.0)
+    sleep(1)
+    driver.set_wheels_speed(-0.2)
+    sleep(1)
+    driver.set_wheels_speed(0)
+except KeyboardInterrupt:
+    driver.set_wheels_speed(0)
